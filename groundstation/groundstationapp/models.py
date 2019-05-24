39333dbd7c2e6d7c9cdabb5a8a8772b91b140784
CodyAnderson <cody@codyanderson.net>
2019-05-24 16:17:21 -0700

    Added basic Packet model and IridiumData model to demo how ForeignKey is done in Django

--- groundstation/groundstationapp/models.py
+++ groundstation/groundstationapp/models.py
@@ -4,3 +4,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Packet(model.Model):
+    id = models.AutoField(primary_key=True)
+    packet_id = models.IntegerField()
+    version = models.IntegerField()
+
+class IridiumData(model.Model):
+    global_id = models.ForeignKey(Packet, on_delete=models.CASCADE)
+    transmit_time = models.DateTimeField()
+    iridium_latitude = models.FloatField()
+    iridium_longitude = models.FloatField()
+    iridium_cep = models.FloatField()
+    momsn = models.IntegerField()
+    imei = models.BigIntegerField()
