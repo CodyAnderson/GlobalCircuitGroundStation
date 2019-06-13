51ebfa644e78cd6b491fedbaada57045d2296433
Collin MacDicken <collin.macdicken@digipen.edu>
2019-06-12 19:15:15 -0700

    Integer division

--- groundstation/groundstationapp/views.py
+++ groundstation/groundstationapp/views.py
@@ -74,8 +74,8 @@ def postfunc(request):
 			print(packet_fields['version'])
 			new_Packet = models.Packet.objects.create(packet_id=packet_fields['seq'],version=packet_fields['version'])
 			new_RawData = models.RawData.objects.create(global_id=new_Packet,data=binary_packet_data,hexdata=packet_data)
-			hour_now = packet_fields['time']/10000
-			minute_now = (packet_fields['time']-hour_now*10000)/100
+			hour_now = packet_fields['time']//10000
+			minute_now = (packet_fields['time']-hour_now*10000)//100
 			second_now = packet_fields['time']-hour_now*10000-minute_now*100
 			time_now = datetime.utcnow().replace(hour=hour_now,minute=minute_now,second=second_now,microsecond=0)
 			new_SlowMeasurement = models.SlowMeasurement.objects.create(global_id=new_Packet,gps_latitude=packet_fields['lat'],gps_longitude=packet_fields['lon'],gps_altitude=packet_fields['alt'],gps_time=time_now)
