7dc529d4633cf9ac956c4d7e1039a695ddaacc14
Collin MacDicken <collin.macdicken@digipen.edu>
2019-07-07 12:22:18 -0700

    finalfinal(?) touches to supervision graph

--- groundstation/groundstationapp/graphs/supervision.py
+++ groundstation/groundstationapp/graphs/supervision.py
@@ -34,12 +34,12 @@ def supervision(getParams):
   chartOptions = {'title': chartTitle}
   onlyWantedData = []
   dataHeader = [
-      [{'type': 'datetime', 'label': 'Time'}, 'valueL', 'valueH', 'sequence']   # create a list to hold the column names and data for the axis names
+      [{'type': 'datetime', 'label': 'Time'}, 'value', 'sequence', 'ID']   # create a list to hold the column names and data for the axis names
     ]
       
   
             
-  supdata = models.SupData.objects.filter(global_id__global_id__transmit_time__gte=minTime).filter(global_id__global_id__transmit_time__lte=maxTime).order_by('global_id')
+  supdata = models.SupData.objects.filter(global_id__global_id__transmit_time__gte=minTime).filter(global_id__global_id__transmit_time__lte=maxTime).order_by('global_id', 'sub_id')
   #print(ordered_fastmeasurements.query)
   top = 99999 if not maxVal else float(maxVal)
   bottom = -99999 if not minVal else float(minVal)
@@ -51,9 +51,12 @@ def supervision(getParams):
       tempDateTime = x.global_id.global_id.transmit_time
       tDTS = tempDateTime.strftime("Date(%Y, %m, %d, %H, %M, %S, %f)")
       tempDateString = tDTS[:11] + '{0:02d}'.format(int(tDTS[11:13])-1) + tDTS[13:31] + '{0:03d}'.format(int(tDTS[31:37])//1000) + tDTS[37:]
-      onlyWantedData.append([tempDateString, x.value[0], x.value[1], x.global_id.packet_id%10])
+      if(((x.type=='Temperature' or x.type=='Pressure') and x.sub_id==0) or x.type=='IL0' or x.type=='IL2' or x.type=='IH1' or x.type=='T0' or x.type=='T2' or x.type=='Tadc1' or x.type=='Text'):
+        onlyWantedData.append([tempDateString, x.value, x.global_id.packet_id%10, 0])
+      else:
+        onlyWantedData.append([tempDateString, x.value, x.global_id.packet_id%10, 1])
   
-  chartOptions["series"] = {0: {"targetAxisIndex": 0},1: {"targetAxisIndex": 0}, 2: {"targetAxisIndex": 1}}
+  chartOptions["series"] = {0: {"targetAxisIndex": 0},1: {"targetAxisIndex": 1}, 2: {"targetAxisIndex": 1}}
   chartOptions["vAxes"] = {0: {"title": 'value'}, 1: {"title": 'sequence'}}    
   data = dataHeader + onlyWantedData
   data_source = SimpleDataSource(data=data)
