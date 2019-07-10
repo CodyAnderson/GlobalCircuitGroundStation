3071e7d36c738c74da86e3a476c168543deebf15
CodyAnderson <cody@codyanderson.net>
2019-07-10 12:57:40 -0700

    🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈

--- groundstation/groundstationapp/newGraph.py
+++ groundstation/groundstationapp/newGraph.py
@@ -199,7 +199,7 @@ def googleMap(request):
   
   points = [] #FORMAT OF '[Lat(float), Long(float), Name(String)],'
   
-  ordered_gpsmeasurements = models.SlowMeasurement.objects.order_by('-global_id__global_id__transmit_time')[20:120]
+  ordered_gpsmeasurements = models.SlowMeasurement.objects.order_by('-global_id__global_id__transmit_time')[:100]
   
   for x in ordered_gpsmeasurements:
     tempDateTime = x.global_id.global_id.transmit_time
