8ebf6f0400e09933bef7e04e36f7e3ff2ddfa3ec
Cody Anderson <cody.anderson@digipen.edu>
2019-08-14 19:06:29 -0700

    Ehhh

--- groundstation/groundstationapp/graphs/horizontal.py
+++ groundstation/groundstationapp/graphs/horizontal.py
@@ -41,7 +41,7 @@ def horizontal(getParams):
 			
 	
 						
-	ordered_fastmeasurements = models.FastMeasurement.objects.annotate(transmit_time=F('global_id__global_id__transmit_time')).annotate(sample_time=ExpressionWrapper(F('transmit_time')+F('sub_id')*timedelta(seconds=5), output_field=DateTimeField())).filter(transmit_time__gte=minTime).filter(transmit_time__lte=maxTime).order_by('global_id', 'sub_id')
+	ordered_fastmeasurements = models.FastMeasurement.objects.annotate(packet_transmit_time=F('global_id__global_id__transmit_time')).annotate(sample_delay=ExpressionWrapper(F('sub_id')*timedelta(seconds=5), output_field=DurationField())).annotate(sample_time=ExpressionWrapper(F('packet_transmit_time')+F('sample_delay'), output_field=DateTimeField())).filter(transmit_time__gte=minTime).filter(transmit_time__lte=maxTime).order_by('global_id', 'sub_id')
 	print(ordered_fastmeasurements.query)
 	print('Annotated time: ' + str(ordered_fastmeasurements[0].sample_time))
 	x = ordered_fastmeasurements[0]
