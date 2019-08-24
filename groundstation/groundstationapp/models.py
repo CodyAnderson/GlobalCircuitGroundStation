34afc97ad60a6f26d2efc67dfc4abaa277636be2
Cody Anderson <cody.anderson@digipen.edu>
2019-08-23 21:13:41 -0700

    Bigger W H O O P S

--- groundstation/groundstationapp/models.py
+++ groundstation/groundstationapp/models.py
@@ -180,7 +180,7 @@ class Measurements(models.Model):
   horizD = models.IntegerField()
   
 class ConductivityMeasurements(models.Model):
-  parent_packet = models.ForeignKey(PacketV9, on_delete=models.CASCADE)
+  parent_packet = models.ForeignKey(PacketV6, on_delete=models.CASCADE)
   #parent_conductivity_packet = models.ForeignKey(Packet, on_delete=models.CASCADE)
   
   time = models.DateTimeField()
