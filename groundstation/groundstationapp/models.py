5ecd9f20631681db84cda4c3f76d62a6d60ff848
Cody Anderson <cody.anderson@digipen.edu>
2019-08-26 17:42:17 -0700

    Added new models for the 'converted' values

--- groundstation/groundstationapp/models.py
+++ groundstation/groundstationapp/models.py
@@ -91,6 +91,7 @@ class Request(models.Model):
   response_errors = models.TextField(null=True)
   response_status_code = models.TextField(null=True)
   
+  
 class IridiumTransmission(models.Model):
   parent_request = models.OneToOneField(Request, related_name='child_transmission', on_delete=models.CASCADE, primary_key=True)
   
@@ -109,11 +110,13 @@ class IridiumTransmission(models.Model):
   
   transmitted_via_satellite = models.BooleanField(default=True)
   
+  
 class RawPacket(models.Model):
   parent_transmission = models.OneToOneField(IridiumTransmission, related_name='child_raw_packet', on_delete=models.CASCADE, primary_key=True)
   
   data = models.BinaryField(null=True)
   hexdata = models.TextField(null=True)
+  
   
 class PacketV6(models.Model):
   parent_transmission = models.OneToOneField(IridiumTransmission, related_name='child_packet', on_delete=models.CASCADE, primary_key=True)
@@ -162,6 +165,42 @@ class PacketV6(models.Model):
   external_temp = models.IntegerField()
   rockblock_temp = models.IntegerField()
   
+class PacketV6Units(models.Model):
+  parent_packet_v6 = models.OneToOneField(IridiumTransmission, related_name='child_packet_v6_units', on_delete=models.CASCADE, primary_key=True)
+  
+  yikes_status = models.TextField(null=True)
+  
+  time = models.DateTimeField()
+  
+  latitude = models.FloatField()
+  longitude = models.FloatField()
+  altitude = models.FloatField()
+  
+  ballast_status = models.TextField(null=True)
+  cutdown_status = models.TextField(null=True)
+  
+  rockblock_signal_strength = models.FloatField()
+  
+  altimeter_temp = models.FloatField()
+  altimeter_pressure = models.FloatField()
+  
+  positive_7v_battery_voltage = models.FloatField()
+  negative_7v_battery_voltage = models.FloatField()
+  
+  positive_3v6_battery_voltage = models.FloatField()
+  
+  current_draw_7v_rail = models.FloatField()
+  current_draw_3v3_rail = models.FloatField()
+  
+  battery_temp = models.FloatField()
+  mcu_temp = models.FloatField()
+  compass_temp = models.FloatField()
+  adc1_temp = models.FloatField()
+  adc2_temp = models.FloatField()
+  external_temp = models.FloatField()
+  rockblock_temp = models.FloatField()
+  
+  
 class Measurements(models.Model):
   parent_packet = models.ForeignKey(PacketV6, on_delete=models.CASCADE)
   
@@ -179,6 +218,24 @@ class Measurements(models.Model):
   horiz2 = models.IntegerField()
   horizD = models.IntegerField()
   
+class MeasurementsUnits(models.Model):
+  parent_measurements = models.OneToOneField(Measurements, related_name='child_measurements_units', on_delete=models.CASCADE, primary_key=True)
+  
+  time = models.DateTimeField()
+  
+  vert1 = models.FloatField()
+  vert2 = models.FloatField()
+  vertD = models.FloatField()
+  
+  compassX = models.FloatField()
+  compassY = models.FloatField()
+  compassZ = models.FloatField()
+  
+  horiz1 = models.FloatField()
+  horiz2 = models.FloatField()
+  horizD = models.FloatField()
+
+
 class ConductivityMeasurements(models.Model):
   parent_packet = models.ForeignKey(PacketV6, on_delete=models.CASCADE)
   #parent_conductivity_packet = models.ForeignKey(Packet, on_delete=models.CASCADE)
@@ -187,4 +244,12 @@ class ConductivityMeasurements(models.Model):
   
   vert1 = models.IntegerField()
   vert2 = models.IntegerField()
+  
+class ConductivityMeasurementsUnits(models.Model):
+  parent_conductivity_measurements = models.OneToOneField(Measurements, related_name='child_conductivity_measurements_units', on_delete=models.CASCADE, primary_key=True)
+  
+  time = models.DateTimeField()
+  
+  vert1 = models.FloatField()
+  vert2 = models.FloatField()
   
