18627165f967a64f9d37e3d112e0735e8c9bbc02
Cody Anderson <cody.anderson@digipen.edu>
2019-08-28 16:36:56 -0700

    Well, I forgot how silly the way to send a datetinme to javascript is, so here is me trying

--- groundstation/groundstationapp/graphV6.py
+++ groundstation/groundstationapp/graphV6.py
@@ -12,6 +12,13 @@ import datetime as dt
 from datetime import datetime
 from datetime import timedelta
 
+def sillyJavascriptDatetimeString(datetimeObject):
+  tDTS = datetimeObject.strftime("Date(%Y, %m, %d, %H, %M, %S, %f)")
+  tempDateString = tDTS[:11] + '{0:02d}'.format(int(tDTS[11:13])-1) + tDTS[13:31] + '{0:03d}'.format(int(tDTS[31:37])//1000) + tDTS[37:]
+  return tempDateString
+
+sJDS = sillyJavascriptDatetimeString
+
 def graphV6(request):
   data = []
   onlyWantedData = []
@@ -22,13 +29,15 @@ def graphV6(request):
   chartDescription = "Measured voltages of the batteries."
   chartOptions = {'title': chartTitle}
   
+  
+  
   uncutData = models.PacketV6Units.objects.all()
   
   dataHeader = [
 			[{'type': 'datetime', 'label': 'Time'}, 'Volts +7V', 'Volts -7V', 'Volts +3V6']	 # create a list to hold the column names and data for the axis names
 		]
     
-  data = [[x.time.strftime("Date(%Y, %m, %d, %H, %M, %S, %f)"), x.positive_7v_battery_voltage, x.negative_7v_battery_voltage, x.positive_3v6_battery_voltage] for x in uncutData]
+  data = [[sJDS(x), x.positive_7v_battery_voltage, x.negative_7v_battery_voltage, x.positive_3v6_battery_voltage] for x in uncutData]
   
   dataList = dataHeader + data
   
