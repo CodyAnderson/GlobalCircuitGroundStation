b6a2c832a5a40095008e9d3e02e20204d310dfd0
CodyAnderson <cody@codyanderson.net>
2019-08-30 17:14:44 -0700

    I wonder how this wacky, long list will work

--- groundstation/groundstationapp/graphV6.py
+++ groundstation/groundstationapp/graphV6.py
@@ -49,7 +49,112 @@ def graphV6(request):
   formFields['IMEI']['options'] = ['ANY', '300234065252710', '300434063219840', '300434063839690', '300434063766960', '300434063560100', '300434063184090', '300434063383330', '300434063185070', '300434063382350', '300234063778640', '888888888888888']
   formFields['IMEI']['selected'] = request.GET.get('IMEI', 'ANY')
   
+  signalList = [
+                'NONE'                                        ,
+                'Request___processing_duration'               ,
+                'Request___forwarded_for_address'             ,
+                'Request___remote_address'                    ,
+                'Request___response_duration'                 ,
+                'Request___response_status_code'              ,
+                
+                
+                
+                'IridiumTransmission___latitude'              ,
+                'IridiumTransmission___longitude'             ,
+                'IridiumTransmission___cep'                   ,
+                'IridiumTransmission___momsn'                 ,
+                'IridiumTransmission___imei'                  ,
+                'IridiumTransmission___device_type'           ,
+                'IridiumTransmission___serial'                ,
+                'IridiumTransmission___iridium_session_status',
+                
+                
+                
+                'PacketV6___yikes_status'                     ,
+                'PacketV6___mcu_id'                           ,
+                'PacketV6___version'                          ,
+                'PacketV6___sequence_id'                      ,
+                'PacketV6___latitude'                         ,
+                'PacketV6___longitude'                        ,
+                'PacketV6___altitude'                         ,
+                'PacketV6___ballast_status'                   ,
+                'PacketV6___cutdown_status'                   ,
+                'PacketV6___conductivity_time'                ,
+                'PacketV6___satellites_count'                 ,
+                'PacketV6___rockblock_signal_strength'        ,
+                'PacketV6___commands_count'                   ,
+                'PacketV6___altimeter_temp'                   ,
+                'PacketV6___altimeter_pressure'               ,
+                'PacketV6___positive_7v_battery_voltage'      ,
+                'PacketV6___negative_7v_battery_voltage'      ,
+                'PacketV6___positive_3v6_battery_voltage'     ,
+                'PacketV6___current_draw_7v_rail'             ,
+                'PacketV6___current_draw_3v3_rail'            ,
+                'PacketV6___battery_temp'                     ,
+                'PacketV6___mcu_temp'                         ,
+                'PacketV6___compass_temp'                     ,
+                'PacketV6___adc1_temp'                        ,
+                'PacketV6___adc2_temp'                        ,
+                'PacketV6___external_temp'                    ,
+                'PacketV6___rockblock_temp'                   ,
+                
+                'PacketV6Units___latitude'                    ,
+                'PacketV6Units___longitude'                   ,
+                'PacketV6Units___altitude'                    ,
+                'PacketV6Units___conductivity_time'           ,
+                'PacketV6Units___satellites_count'            ,
+                'PacketV6Units___rockblock_signal_strength'   ,
+                'PacketV6Units___commands_count'              ,
+                'PacketV6Units___altimeter_temp'              ,
+                'PacketV6Units___altimeter_pressure'          ,
+                'PacketV6Units___positive_7v_battery_voltage' ,
+                'PacketV6Units___negative_7v_battery_voltage' ,
+                'PacketV6Units___positive_3v6_battery_voltage',
+                'PacketV6Units___current_draw_7v_rail'        ,
+                'PacketV6Units___current_draw_3v3_rail'       ,
+                'PacketV6Units___battery_temp'                ,
+                'PacketV6Units___mcu_temp'                    ,
+                'PacketV6Units___compass_temp'                ,
+                'PacketV6Units___adc1_temp'                   ,
+                'PacketV6Units___adc2_temp'                   ,
+                'PacketV6Units___external_temp'               ,
+                'PacketV6Units___rockblock_temp'              ,
+                
+                
+                
+                'Measurements___vert1'                        ,
+                'Measurements___vert2'                        ,
+                'Measurements___vertD'                        ,
+                'Measurements___compassX'                     ,
+                'Measurements___compassY'                     ,
+                'Measurements___compassZ'                     ,
+                'Measurements___horiz1'                       ,
+                'Measurements___horiz2'                       ,
+                'Measurements___horizD'                       ,
+                
+                'MeasurementsUnits___vert1'                   ,
+                'MeasurementsUnits___vert2'                   ,
+                'MeasurementsUnits___vertD'                   ,
+                'MeasurementsUnits___compassX'                ,
+                'MeasurementsUnits___compassY'                ,
+                'MeasurementsUnits___compassZ'                ,
+                'MeasurementsUnits___horiz1'                  ,
+                'MeasurementsUnits___horiz2'                  ,
+                'MeasurementsUnits___horizD'                  ,
+                
+                
+                
+                'ConductivityMeasurements___vert1'            ,
+                'ConductivityMeasurements___vert2'            ,
+                
+                'ConductivityMeasurementsUnits___vert1'       ,
+                'ConductivityMeasurementsUnits___vert2'       ,
+                ]
   
+  formFields['leftAxisSignal_A'] = {}
+  formFields['leftAxisSignal_A']['label'] = 'Left Axis Signal A'
+  formFields['leftAxisSignal_A']['options'] = signalList
+  formFields['leftAxisSignal_A']['selected'] = request.GET.get('leftAxisSignal_A', 'ANY')
   
   chartTitle = "Battery Voltage"
   chartDescription = "Measured voltages of the batteries."
