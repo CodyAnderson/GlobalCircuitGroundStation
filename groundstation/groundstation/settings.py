04d1d6c970cb13f3569cc2f4ddeb9241a6b6f109
CodyAnderson <cody@codyanderson.net>
2019-09-02 10:38:10 -0700

    UNDERSCORE

--- groundstation/groundstation/settings.py
+++ groundstation/groundstation/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'background-task',
+    'background_task',
     'groundstationapp',
     'graphos',
     'corsheaders',
