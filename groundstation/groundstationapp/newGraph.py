403c237a0b73036415f53d804562d0f5aff65b25
CodyAnderson <cody@codyanderson.net>
2019-09-02 23:13:36 -0700

    no message

--- groundstation/groundstationapp/newGraph.py
+++ groundstation/groundstationapp/newGraph.py
@@ -310,7 +310,7 @@ def googleMap(request):
   if(formFields['IMEI']['selected'] != 'ANY'):
     ordered_gpsmeasurements = ordered_gpsmeasurements.filter(parent_packet_v6__parent_transmission__imei=int(formFields['IMEI']['selected']))
   
-  ordered_gpsmeasurements = ordered_gpsmeasurements[:50]
+  ordered_gpsmeasurements = ordered_gpsmeasurements[:400]
   
   for x in ordered_gpsmeasurements:
     tempDateTime = x.time
